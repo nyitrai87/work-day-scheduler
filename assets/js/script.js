fa51071f4e0fd7012104373041eb919e16a30fcc
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-01-08 22:28:57 +0000

    timeblocks are colourised according to the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,20 @@
 const currentDay = dayjs();
-$("#currentDay").text(currentDay.format("dddd, MMMM DD"));
+$('#currentDay').text(currentDay.format('dddd, MMMM DD'));
+const currentHour = dayjs().format('HH')
+
+const timeblockEl = $('.time-block');
+const hourEl = $('.hour');
+const descriptionEL = $('.description');
+const saveBtnEl = $('.saveBtn');
+
+timeblockEl.each(function () {
+    let hourNum = $(this).attr('data-hour');
+
+    if (currentHour === hourNum) {
+        $(this).addClass("present");
+    } else if (currentHour < hourNum) {
+        $(this).addClass("future");
+    } else if (currentHour > hourNum) {
+        $(this).addClass("past");
+    }
+})
